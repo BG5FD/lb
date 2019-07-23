ce2bb4d37eb05b6d86c5cbfa8a97a45f91efefe4
gaohaozhen <gaohaozhen@cnpc.com.cn>
2019-07-23 22:53:15 +0800

    增加时间安排

--- src/routes/timeline/index.jsx
+++ src/routes/timeline/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import moment from 'moment';
 import {
 	withRouter,
 } from 'react-router-dom';
+import {
+	Icon, Button, Timeline,
+} from 'antd';
+import {
+	TIMELINE,
+} from 'utils/enum';
 
 class Index extends React.Component {
 	constructor(props) {
@@ -9,13 +16,48 @@ class Index extends React.Component {
 		this.state = {};
 	}
 
+	getColor = (obj) => {
+		if (Date.now() < obj.start) {
+			return 0;
+		} else if (Date.now() <= obj.end) {
+			return 1;
+		} else {
+			return 2;
+		}
+	}
+
 	render() {
 		return (
 			<div>
-				待开发
+				<Button onClick={() => this.props.history.goBack()} style={{ margin: 10 }} type="primary">
+					<Icon type="arrow-left"/>
+					<span>返回</span>
+				</Button>
+				<Timeline className="lb-timeline">
+					{
+						TIMELINE.map(each => {
+							return this.getColor(each) === 1 ? (
+								<Timeline.Item
+									color="blue"
+									dot={<Icon style={{ fontSize: '16px' }} type="clock-circle-o" />}
+									key={each.start}
+								>
+									{`${moment(each.start).format('MM-DD HH:mm')}-${moment(each.end).format('MM-DD HH:mm')}：${each.text}`}
+								</Timeline.Item>
+							): (
+								<Timeline.Item
+									color={this.getColor(each) < 1 ? 'green' : 'blue'}
+									key={each.start}
+								>
+									{`${moment(each.start).format('MM-DD HH:mm')}-${moment(each.end).format('MM-DD HH:mm')}：${each.text}`}
+								</Timeline.Item>
+							);
+						})
+					}
+				</Timeline>
 			</div>
 		);
 	}
 }
 
-export default Index;
+export default withRouter(Index);
--- src/utils/enum.js
+++ src/utils/enum.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export const MENU = [{
 	key: 'BRIEF',
 	title: '活动简介',
@@ -18,4 +20,42 @@ export const MENU = [{
 	key: 'ACTIVITY',
 	title: '大巴活动',
 	uri: '/activity',
+}];
+
+export const TIMELINE = [{
+	start: moment('2019-07-26 13:30'),
+	end: moment('2019-07-26 18:30'),
+	text: '乘坐大巴前往目的地（车上团建小活动）',
+}, {
+	start: moment('2019-07-26 19:00'),
+	end: moment('2019-07-26 24:00'),
+	text: '晚饭、饭后娱乐（棋牌、麻将、ktv等）',
+}, {
+	start: moment('2019-07-27 8:30'),
+	end: moment('2019-07-27 9:00'),
+	text: '早餐',
+}, {
+	start: moment('2019-07-27 9:10'),
+	end: moment('2019-07-27 11:30'),
+	text: '草原骑马、草原小游戏',
+}, {
+	start: moment('2019-07-27 12:00'),
+	end: moment('2019-07-27 14:00'),
+	text: '午餐、午休',
+}, {
+	start: moment('2019-07-27 14:20'),
+	end: moment('2019-07-27 17:30'),
+	text: '皇家鹿苑',
+}, {
+	start: moment('2019-07-27 18:00'),
+	end: moment('2019-07-27 24:00'),
+	text: '特色烤全羊，各种烧烤、啤酒、农家饭等等。饭后娱乐（棋牌、麻将、ktv等）',
+}, {
+	start: moment('2019-07-28 8:00'),
+	end: moment('2019-07-28 9:00'),
+	text: '早餐',
+}, {
+	start: moment('2019-07-28 9:00'),
+	end: moment('2019-07-28 14:00'),
+	text: '乘坐大巴返程',
 }];
